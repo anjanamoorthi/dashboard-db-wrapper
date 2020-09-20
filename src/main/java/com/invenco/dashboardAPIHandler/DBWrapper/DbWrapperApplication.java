6158d5e5e4e1bfb43cf9a54e28a9b52a47a8b888
Moorthi <Moorthi.Rajan@invenco.com>
2020-09-21 10:12:39 +1200

    Hack-42 Init repo for db microservice

---
+++ src/main/java/com/invenco/dashboardAPIHandler/DBWrapper/DbWrapperApplication.java
@@ -0,0 +1,13 @@
+package com.invenco.dashboardAPIHandler.DBWrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbWrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbWrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invenco/dashboardAPIHandler/DBWrapper/DbWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invenco.dashboardAPIHandler.DBWrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbWrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
